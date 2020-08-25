ab7cc380eaeaa27956bfc63f428dd17acb804fd3
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-25 05:00:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song = self.new(filename)
+    song = self.new
     song.name = filename.split(" - ")[0]
     song.artist = filename.split(" - ")[1]
     song
