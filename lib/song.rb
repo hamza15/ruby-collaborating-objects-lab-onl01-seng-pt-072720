edc391168dfd820a009f6b95da1a779c21b7e5d6
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-25 04:06:07 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+  
+  attr_accessor :name, :artist
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    
+  end 
+
+end
