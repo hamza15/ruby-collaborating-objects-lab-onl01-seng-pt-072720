7ba65f13084215b2decfa88ee260c0dd60710154
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-25 04:22:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,7 @@ class Artist
   def self.find_or_create_by_name(name)
     if self.all.find { |artist| artist.name == name }
     else
-      self.name = Artist.new(name)
+      artist = Artist.new(name)
     end
   end 
       
